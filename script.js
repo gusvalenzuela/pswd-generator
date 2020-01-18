29f38767f3721bf73540ee5b698a04998fa30978
G Val <13578427+gusvalenzuela@users.noreply.github.com>
2020-01-18 01:39:46 -0800

    replaced text input with slider

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const input = document.querySelector(`#passwordNum`)
+const slider = document.querySelector(`#pswd-length`)
+const sliderVal = document.querySelector(`#val-holder`)
 const submitBtn = document.querySelector(`#submit`)
 const contentHolder = document.querySelector(`#content-here`)
 const inputOptions = document.querySelector(`#inputs`)
@@ -6,25 +7,24 @@ const lCaseString = `abcdefghijklmnopqrstuvwxyz`
 const uCaseString = lCaseString.toUpperCase() 
 const spCharsString = `~!@#$%^&*()_` // only top bar special characters
 const numString = `0123456789`
-var userNum = 0;
 
+sliderVal.textContent = slider.value
+
+slider.oninput = function() {
+    sliderVal.textContent = slider.value;
+  }
 
 uCaseCheck = document.querySelector(`#upper-case`)
 lCaseCheck = document.querySelector(`#lower-case`)
 spCheck = document.querySelector(`#special-characters`)
 numCheck = document.querySelector(`#numbers`)
 
-input.addEventListener('input', updateValue);
 
 // if(userNum >= 8 && userNum <= 128){
 //     input.setAttribute(`style`, `border: 2px green`)        
 // } else {
 //     input.setAttribute(`style`, `border: 2px red`)  
 // }
-
-function updateValue(e) {
-    userNum = e.target.value;
-}
 
 submitBtn.addEventListener(`click`, runGenerator)
 
@@ -33,12 +33,13 @@ function runGenerator(){
     var currentString = ""
     var generatedPswd = ""
 
-    // four straight if statements checking to see which checkboxes have been checked
-    // and adding them to the currentString to use for grabbing characters in the generator
+    // validating that at least one option has been checked 
     if((uCaseCheck.checked || lCaseCheck.checked || spCheck.checked || numCheck.checked)){
+        // four straight if statements checking to see which checkboxes have been checked
+        // and adding them to the currentString to use for grabbing characters in the generator
         if(uCaseCheck.checked){
             currentString = currentString + uCaseString
-        }git 
+        }
         if(lCaseCheck.checked){
             currentString = currentString + lCaseString
         }   
