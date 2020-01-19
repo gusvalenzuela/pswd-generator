8e0dfe38027e3cc044876da2f497b484d09dfb72
G Val <13578427+gusvalenzuela@users.noreply.github.com>
2020-01-18 20:46:43 -0800

    added bootstrap and bunch of formatting. updates to README

--- script.js
+++ script.js
@@ -1,48 +1,57 @@
 const slider = document.querySelector(`#pswd-length`)
 const sliderVal = document.querySelector(`#val-holder`)
 const submitBtn = document.querySelector(`#submit`)
-const contentHolder = document.querySelector(`#content-here`)
+const pswdHolder = document.querySelector(`#password-holder`)
 const inputOptions = document.querySelector(`#inputs`)
 const lCaseString = `abcdefghijklmnopqrstuvwxyz`
 const uCaseString = lCaseString.toUpperCase() 
 const spCharsString = `~!@#$%^&*()_` // only top bar special characters
 const numString = `0123456789`
+const msg = document.querySelector(`#display-msg`)
 
-sliderVal.textContent = slider.value
+// copy to Clipboard 
 
+
+// grabbing all the checkboxes and their bootstrap elements
+uCaseCheck = document.querySelector(`#upper-case`)
+lCaseCheck = document.querySelector(`#lower-case`)
+spCheck = document.querySelector(`#special`)
+numCheck = document.querySelector(`#numbers`)
+uCaseCheckBS = document.querySelector(`#upper-case-bs`)
+lCaseCheckBS = document.querySelector(`#lower-case-bs`)
+spCheckBS = document.querySelector(`#special-bs`)
+numCheckBS = document.querySelector(`#numbers-bs`)
+
+init ()
+
+function init (){
+    sliderVal.textContent = slider.value // displaying default value
+    
+
+}
+
+// updating displayed value when value is changed (slider moved)
 slider.oninput = function() {
     sliderVal.textContent = slider.value;
   }
 
-uCaseCheck = document.querySelector(`#upper-case`)
-lCaseCheck = document.querySelector(`#lower-case`)
-spCheck = document.querySelector(`#special-characters`)
-numCheck = document.querySelector(`#numbers`)
-
-
-// if(userNum >= 8 && userNum <= 128){
-//     input.setAttribute(`style`, `border: 2px green`)        
-// } else {
-//     input.setAttribute(`style`, `border: 2px red`)  
-// }
-
-submitBtn.addEventListener(`click`, runGenerator)
-
+// the generator
 function runGenerator(){
-    // event.preventDefault()
     var currentString = ""
     var generatedPswd = ""
 
+    
     // validating that at least one option has been checked 
-    if((uCaseCheck.checked || lCaseCheck.checked || spCheck.checked || numCheck.checked)){
-        // four straight if statements checking to see which checkboxes have been checked
-        // and adding them to the currentString to use for grabbing characters in the generator
-        if(uCaseCheck.checked){
-            currentString = currentString + uCaseString
-        }
+    if(uCaseCheck.checked || lCaseCheck.checked || spCheck.checked || numCheck.checked){
+        msg.textContent = ""
+        // checking to see which checkboxes have been checked
+        // and adding relevant options subsection to the currentString to use for grabbing characters in the generator
         if(lCaseCheck.checked){
             currentString = currentString + lCaseString
         }   
+        if(uCaseCheck.checked){
+            currentString = currentString + uCaseString
+        }
         if(spCheck.checked){
             currentString = currentString + spCharsString
         }
@@ -50,28 +59,28 @@ function runGenerator(){
             currentString = currentString + numString
         }
     } else {
-        console.log(`no boxes have been checked`)
+        msg.textContent = "Please select at least 1 type of character"
+        return
+        // console.log(lCaseCheck.checked)
+        // return
     }
 
-    // checking to see that the user input is between 8 and 128
-    // otherwise alerting them to enter a valid number
-    if(userNum >= 8 && userNum <= 128){
-        input.classList.add(`success`)  
-        
-        // creating a random number used to pull from the currentString by index 
-        for(i=0;i<userNum;i++){
-            var j = Math.floor((Math.random()*currentString.length)+1)
-            generatedPswd = generatedPswd + currentString[j]
-        }
-        
-        // checking the magic 
-        console.log(generatedPswd)
-    } else{
-        input.classList.add(`failure`) 
-        console.log(`You must enter a valid number (between 8 - 128)`)
+    console.log("The current string length is: " + currentString.length)
+    
+    // creating a random number used to pull from the currentString by index 
+    for(i=0;i<slider.value;i++){
+        var j = Math.floor((Math.random()*currentString.length))
+        generatedPswd = generatedPswd + currentString[j]
     }
     
+    // checking the magic 
+    console.log("password is: " + generatedPswd) 
+    pswdHolder.textContent = generatedPswd
 }
 
+// listening for click on my submit button to trigger password generator
+submitBtn.addEventListener(`click`, runGenerator)
 
-// userNum = prompt("Please ENTER desired length of password (must be between: 8 - 128)")
+
+
+        // userNum = prompt("Please ENTER desired length of password (must be between: 8 - 128)")   
