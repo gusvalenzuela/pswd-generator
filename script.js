896384fa6c76895d9f021ec9458d0a8c93f0ccc3
G Val <13578427+gusvalenzuela@users.noreply.github.com>
2020-01-18 23:33:15 -0800

    tweaked the visuals for options validation

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const slider = document.querySelector(`#pswd-length`)
 const sliderVal = document.querySelector(`#val-holder`)
 const submitBtn = document.querySelector(`#submit`)
-const pswdHolder = document.querySelector(`#password-holder`)
+const pswdHolder = document.querySelector(`#generated-password`)
+const pswdDiv = document.querySelector(`#password-holder`)
+const optionsList = document.querySelector(`.options-list`)
 const inputOptions = document.querySelector(`#inputs`)
 const lCaseString = `abcdefghijklmnopqrstuvwxyz`
 const uCaseString = lCaseString.toUpperCase() 
-const spCharsString = `~!@#$%^&*()_` // only top bar special characters
+const spCharsString = `!"#$%&'()*+,-./:;<=>?@[]^_{|}~` // removed space, \, and `
 const numString = `0123456789`
 const msg = document.querySelector(`#display-msg`)
-
-// copy to Clipboard 
-
 
 // grabbing all the checkboxes and their bootstrap elements
 uCaseCheck = document.querySelector(`#upper-case`)
@@ -30,20 +29,40 @@ function init (){
 
 }
 
+// copy to Clipboard 
+
 // updating displayed value when value is changed (slider moved)
 slider.oninput = function() {
     sliderVal.textContent = slider.value;
-  }
+}
+
+function isCheckboxSelected(){
+    event.stopPropagation()
+    var el = event.target
+
+    console.log(el)
+    
+    if (el.type === `checkbox`) {
+        if(uCaseCheck.checked || lCaseCheck.checked || spCheck.checked || numCheck.checked){
+            msg.textContent = ""
+            optionsList.setAttribute(`style`, ``)
+        } else {
+            msg.textContent = "* must select one *"
+            optionsList.setAttribute(`style`, `border: 4px groove red;`)
+        }
+    }    
+    
+}
 
 // the generator
 function runGenerator(){
     var currentString = ""
     var generatedPswd = ""
 
-    
+    pswdHolder.textContent = ""
+
     // validating that at least one option has been checked 
     if(uCaseCheck.checked || lCaseCheck.checked || spCheck.checked || numCheck.checked){
-        msg.textContent = ""
         // checking to see which checkboxes have been checked
         // and adding relevant options subsection to the currentString to use for grabbing characters in the generator
         if(lCaseCheck.checked){
@@ -59,13 +78,10 @@ function runGenerator(){
             currentString = currentString + numString
         }
     } else {
-        msg.textContent = "Please select at least 1 type of character"
+        msg.textContent = "* must select one *"
+        optionsList.setAttribute(`style`, `border: 4px groove red;`)
         return
-        // console.log(lCaseCheck.checked)
-        // return
-    }
-
-    console.log("The current string length is: " + currentString.length)
+    } 
     
     // creating a random number used to pull from the currentString by index 
     for(i=0;i<slider.value;i++){
@@ -81,6 +97,6 @@ function runGenerator(){
 // listening for click on my submit button to trigger password generator
 submitBtn.addEventListener(`click`, runGenerator)
 
+optionsList.addEventListener(`click`, isCheckboxSelected)
 
-
-        // userNum = prompt("Please ENTER desired length of password (must be between: 8 - 128)")   
+// userNum = prompt("Please ENTER desired length of password (must be between: 8 - 128)")   
